a243fd30ba4639436bd7a9c2683e179771b9a18d
Steven Aubertin <stevenaubertin@gmail.com>
2019-02-13 16:58:59 -0500

    Fix sh variables in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-IMAGE_NAME = x0r/honeycomb
-CONTAINER_NAME = honeycomb
+IMAGE_NAME=x0r/honeycomb
+CONTAINER_NAME=honeycomb
 
-docker build -t $(IMAGE_NAME) .
-docker run -p 5000:5000 --name $(CONTAINER_NAME) $(IMAGE_NAME)
+docker build -t $IMAGE_NAME .
+docker run -p 5000:5000 --name $CONTAINER_NAME $IMAGE_NAME
