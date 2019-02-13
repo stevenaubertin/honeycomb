1bf1b4c1ce9cfdcdf33fe0b868e348493cb87eda
Steven Aubertin <stevenaubertin@gmail.com>
2019-02-13 17:30:46 -0500

    Change log filename

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     """Base class for configuration"""
     DEBUG = False
     TESTING = False
-    LOG_FILENAME = 'wol.log'
+    LOG_FILENAME = 'log'
     LOG_MAX_BYTES = 10000
     LOG_BACKUP_COUNT = 1
     LOG_LEVEL = ['INFO', 'ERROR']
