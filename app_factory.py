b5c65eaba417f95fb304c98e2bdcdd2b3c92546a
Steven Aubertin <stevenaubertin@gmail.com>
2019-02-13 18:58:48 -0500

    Fix RotatingFileHandler

--- app_factory.py
+++ app_factory.py
@@ -9,27 +9,15 @@ from logging.handlers import RotatingFileHandler
 def create_app(name, config='config.ProductionConfig'):
     app = Flask(name)
     app.config.from_object(config)
+    
+    app.logger = logging.getLogger(name)
+    if 'LOG_LEVEL' in app.config:
+        app.logger.setLevel(app.config['LOG_LEVEL'])
+    
+    app.logger.addHandler(RotatingFileHandler(
+        app.config['LOG_FILENAME'],
+        maxBytes=app.config['LOG_MAX_BYTES'],
+        backupCount=app.config['LOG_BACKUP_COUNT']
+    ))
 
-    if 'LOG_LEVELS' in app.config:
-        for level in app.config['LOG_LEVELS']:
-            handler = RotatingFileHandler(
-                app.config['LOG_FILENAME'],
-                maxBytes=app.config['LOG_MAX_BYTES'],
-                backupCount=app.config['LOG_BACKUP_COUNT']
-            )
-            
-            if level == 'INFO':
-                handler.setLevel(logging.INFO)
-            elif level == 'CRITICAL':
-                handler.setLevel(logging.CRITICAL)
-            elif level == 'DEBUG':
-                handler.setLevel(logging.DEBUG)
-            elif level == 'ERROR':
-                handler.setLevel(logging.ERROR)
-            elif level == 'FATAL':
-                handler.setLevel(logging.FATAL)
-            else:
-                raise Exception('Invalid logging level')
-
-            app.logger.addHandler(handler)
     return app
