ab58a77bd1f3dece5dbefaaf2ea8594abe1ea7c8
Steven Aubertin <stevenaubertin@gmail.com>
2019-02-13 19:17:38 -0500

    Make the application fetch config from environnement variable

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ class Config(object):
     ENV = 'development'
     DEBUG = False
     TESTING = False
-    LOG_FILENAME = 'app.log'
+    LOG_FILENAME = os.environ.get('LOG_FILENAME', 'app.log')
     LOG_MAX_BYTES = 10000
     LOG_BACKUP_COUNT = 1
     LOG_LEVEL = logging.INFO
     LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    HOST = "0.0.0.0"
+    HOST = os.environ.get('HOST', '0.0.0.0')
     PORT = os.environ.get('PORT', 5000)
     SECRET_KEY = None
 
